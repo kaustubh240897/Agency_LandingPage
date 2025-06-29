a9d5243a6724112d8da583559b2a9511897ba95a
Kaustubh Dwivedi <31632607+kaustubh240897@users.noreply.github.com>
2025-06-29 17:20:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: isProd ? './' : '',
+  trailingSlash: true,
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
